c3d9f93f0518469f9f249d706f4692dd3fb2ebdd
Scott Yamamoto <scott.yamamoto@hotmail.com>
2019-01-27 00:48:18 -0600

    Copied a MERN tutorial

---
+++ client/src/CommentList.mjs
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Comment from './Comment';
+
+const CommentList = (props) => {
+  const commentNodes = props.data.map(comment => (
+    <Comment author={comment.author} key={comment._id} id={comment._id}>
+      { comment.text}
+    </Comment>
+  ));
+  return (
+    <Comment
+  author={comment.author}
+  key={comment._id}
+  id={comment._id}
+  timestamp={comment.updatedAt}
+>
+  );
+};
+
+CommentList.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({
+    author: PropTypes.string,
+    id: PropTypes.string,
+    text: PropTypes.string,
+  })),
+};
+
+CommentList.defaultProps = {
+  data: [],
+};
+
+export default CommentList;
